f26b69afd424d3e7f45ceecf4fccc6e52f9f379b
Jordan Kostelidis <JordanKostelidis@TechLimitTV.eu>
2016-11-21 19:27:07 +0200

    Register (By AntonisHopkins)

---
+++ Talos Quests/app/shared/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	apiUrl: "http://service.talosquests.devian.gr/"
+};
+/*module.exports = {
+    apiUrl: "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/"
+};*/
---
+++ Talos Quests/app/views/register/register.js
@@ -0,0 +1,44 @@
+var dialogsModule = require("ui/dialogs");
+var frameModule = require("ui/frame");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var Observable = require("data/observable").Observable;
+var user = new UserViewModel();
+var page;
+/*var user = new UserViewModel({
+    email: email,
+    password: password,
+    name: name
+});*/
+var indicator = new Observable({
+    isLoading: true,
+    isItemVisible: false
+});
+exports.loaded = function(args) {
+     page = args.object;
+};
+exports.register = function() {
+    page.bindingContext= indicator;
+    completeRegistration();
+};
+function hideIndicator(){
+    indicator.isLoading=false;
+    indicator.isItemVisible=true;
+    page.bindingContext= indicator;
+};
+function completeRegistration() {
+    user.register().then(function() {
+            hideIndicator()
+            dialogsModule.alert("Your account was successfully created.")
+                .then(function() {
+                    frameModule.topmost().navigate("views/test/test");
+                });
+        }).catch(function(error) {
+            console.log(error);
+            hideIndicator()
+            dialogsModule
+                .alert({
+                    message: "Unfortunately we were unable to create your account.",
+                    okButtonText: "OK"
+                });
+        });
+}
