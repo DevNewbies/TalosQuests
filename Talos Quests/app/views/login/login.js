0ca47e07fec4d96bd2f0620f2b6a5077a23d2ea9
Antonis <tonymis.16@hotmail.com>
2016-12-05 16:56:24 +0200

    Login Finished! Communicae with the server

--- Talos Quests/app/views/login/login.js
+++ Talos Quests/app/views/login/login.js
@@ -1,44 +1,66 @@
 var dialogsModule = require("ui/dialogs");
 var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
 var config = require("../../shared/config");
 var UserViewModel = require("../../shared/view-models/user-view-model");
 var Observable = require("data/observable").Observable;
 var user = new UserViewModel();
 var page;
-var indicator = new Observable({
-    isLoading: true,
-    isItemVisible: false
-});
-/*indicator.hideIndicator = function() {
-    indicator.isLoading=false;
-    indicator.isItemVisible=true;
-    page.bindingContext= indicator;
-};*/
 
 exports.loaded = function(args) {
     page = args.object;
+    page.bindingContext = user;
 };
 function hideIndicator(){
-    indicator.isLoading=false;
-    indicator.isItemVisible=true;
-    page.bindingContext= indicator;
+    user.isLoading= false;
+    user.isItemVisible= true;
 };
-exports.signIn = function() {
-    page.bindingContext= indicator;
-    user.login().catch(function(error){
-            hideIndicator()
-            console.log(error);
-            dialogsModule.alert({
-                message: "Username or password are incorrect.",
-                okButtonText: "OK"
-            });
-            return Promise.reject();
-        })
-        .then(function() {
-            hideIndicator()
-            frameModule.topmost().navigate("views/main/main");
+function showIndicator(){
+    user.isLoading=true;
+    user.isItemVisible=false;
+};
+exports.signIn = function() 
+{
+    showIndicator(); 
+    user.login().catch(function(error) {
+      /* console.log(error);
+        console.log(error.message);*/
+             if(error.message === "401")
+            {
+                hideIndicator();
+                dialogsModule.alert({
+                    message: "Username or password is incorrect!",
+                    okButtonText: "OK"
+                });
+            }
+            else if(error.message=== "400")
+            {
+                hideIndicator();
+                dialogsModule.alert({
+                    message: "You did not filled the fields!",
+                    okButtonText: "OK"
+                });
+            }
+            else if(error.message === "500")
+            {
+                hideIndicator();
+                dialogsModule.alert({
+                    message: "Server is busy right now, please try again later!",
+                    okButtonText: "OK"
+                });
+            }
+        return Promise.reject();
+        }).then(function(r){
+            //console.log(JSON.stringify(r));
+                hideIndicator();
+                if(r.state==200){
+                frameModule.topmost().navigate("views/main/main");
+            }
         });
 };
+exports.fbSignIn = function(){
+
+}
 exports.register = function() {
     frameModule.topmost().navigate("views/register/register");
 };
