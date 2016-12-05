2d25c53f0a0354316b95a592c3ac6da2a37575ab
Jordan Kostelidis <iordkost@teicm.gr>
2016-12-05 22:01:22 +0200

    Main Menu without Tests

--- Talos Quests/app/views/main/main-view-model.js
+++ Talos Quests/app/views/main/main-view-model.js
@@ -2,6 +2,10 @@ var Observable = require("data/observable").Observable;
 
 function createViewModel() {
     var viewModel = new Observable();
+
+    var MainMenu = ["Start Game", "My Avatar", "Archivements", "Settings", "Exit"];
+    viewModel.MainMenu = MainMenu;
+
     return viewModel;
 }
 
--- Talos Quests/app/views/main/main.js
+++ Talos Quests/app/views/main/main.js
@@ -1,7 +1,76 @@
 var createViewModel = require("./main-view-model").createViewModel;
 
+var Status = "Null";
+exports.Status = Status;
+
 function onNavigatingTo(args) {
     var page = args.object;
     page.bindingContext = createViewModel();
 }
 exports.onNavigatingTo = onNavigatingTo;
+
+function RunTrigger(IndexOf) {
+    switch(IndexOf) {
+        case 0: {
+            Status = "Start Game";
+            alert("Start Game - Triggered");
+            StartGameProcess();
+            break;
+        }
+        case 1: {
+            Status = "My Avatar";
+            alert("My Avatar - Triggered");
+            MyAvatarProcess();
+            break;
+        }
+        case 2: {
+            Status = "Achievements";
+            alert("Achievements - Triggered");
+            AchievementsProcess();
+            break;
+        }
+        case 3: {
+            Status = "Settings";
+            alert("Settings - Triggered");
+            SettingsProcess();
+            break;
+        }
+        case 4: {
+            Status = "Exit";
+            ExitProcess();
+            break;
+        }
+        default: {
+            Status = "Default";
+            break;
+        }
+    }
+    exports.Status = Status;
+}
+exports.RunTrigger = RunTrigger;
+
+
+function StartGameProcess() {
+	// topmost.navigate("views/startgame/startgame");
+}
+
+function MyAvatarProcess() {
+	// topmost.navigate("views/myavatar/myavatar");
+}
+
+function AchievementsProcess() {
+	// topmost.navigate("views/achievements/achievements");
+}
+
+function SettingsProcess() {
+	// topmost.navigate("views/settings/settings");
+}
+
+function ExitProcess() {
+	android.os.Process.killProcess(android.os.Process.myPid());
+}
+
+function MainMenuTap (TappedItem) {
+    RunTrigger(TappedItem.index)
+}
+exports.MainMenuTap = MainMenuTap;
