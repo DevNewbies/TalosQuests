ac28569f1febda72b5e5fc1cb60a36a21eca1f20
Jordan Kostelidis <JordanKostelidis@TechLimitTV.eu>
2016-12-18 20:19:20 +0200

    Merge main-view-model.js on main.js

--- Talos Quests/app/views/main/main.js
+++ Talos Quests/app/views/main/main.js
@@ -1,11 +1,19 @@
-var createViewModel = require("./main-view-model").createViewModel;
+// Init View
+var Observable = require("data/observable").Observable;
+var CurrentPage;
 
-var Status = "Null";
+var Status = "JustStarted";
 exports.Status = Status;
 
+// Define Menu's Data
+var Menu = new Observable({
+    MenuItems : ["Start Game", "My Avatar", "Archivements", "Settings", "Exit"]
+});
+
+// Unit Functions
 function onNavigatingTo(args) {
-    var page = args.object;
-    page.bindingContext = createViewModel();
+    CurrentPage = args.object;
+    CurrentPage.bindingContext = Menu;
 }
 exports.onNavigatingTo = onNavigatingTo;
 
@@ -13,42 +21,43 @@ function RunTrigger(IndexOf) {
     switch(IndexOf) {
         case 0: {
             Status = "Start Game";
-            alert("Start Game - Triggered");
+            alert(Status);
             StartGameProcess();
             break;
         }
         case 1: {
             Status = "My Avatar";
-            alert("My Avatar - Triggered");
+            alert(Status);
             MyAvatarProcess();
             break;
         }
         case 2: {
             Status = "Achievements";
-            alert("Achievements - Triggered");
+            alert(Status);
             AchievementsProcess();
             break;
         }
         case 3: {
             Status = "Settings";
-            alert("Settings - Triggered");
+            alert(Status);
             SettingsProcess();
             break;
         }
         case 4: {
             Status = "Exit";
+            alert(Status);
             ExitProcess();
             break;
         }
         default: {
             Status = "Default";
+            alert(Status);
             break;
         }
     }
     exports.Status = Status;
 }
 exports.RunTrigger = RunTrigger;
-
 
 function StartGameProcess() {
 	// topmost.navigate("views/startgame/startgame");
@@ -70,7 +79,8 @@ function ExitProcess() {
 	android.os.Process.killProcess(android.os.Process.myPid());
 }
 
-function MainMenuTap (TappedItem) {
-    RunTrigger(TappedItem.index)
+// UI Functions
+function MainMenuTap(TappedItem) {
+  RunTrigger(TappedItem.index);
 }
 exports.MainMenuTap = MainMenuTap;
