47b97b883f23c840c196df93bf88bebafc8f6dba
Nikolas <nikmaurop@gmail.com>
2017-01-11 05:27:11 +0200

    Initial Commit

---
+++ TalosQuestsAdministrationPanel/Model/AuthRegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalosQuestsAdministrationPanel.Model
+{
+    public class AuthRegisterModel
+    {
+        public String userName { get; set; }
+        public String passWord { get; set; }
+        public String email { get; set; }
+        public String imei { get; set; }
+    }
+}
---
+++ TalosQuestsAdministrationPanel/Model/Duration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalosQuestsAdministrationPanel.Model
+{
+    public class Duration
+    {
+        public long inSeconds { get; set; }
+    }
+}
