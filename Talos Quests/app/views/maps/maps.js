c0037be9561869dd1331cbf9da6fd7857c7a2cf8
Danis Kostelidis <iordkost@teicm.gr>
2017-01-13 21:58:09 +0200

    FIX, Map now shows the real Pos.

--- Talos Quests/app/views/maps/maps.js
+++ Talos Quests/app/views/maps/maps.js
@@ -4,17 +4,12 @@ var GeoLocation = require("nativescript-geolocation");
 var mapsModule = require("nativescript-google-maps-sdk");
 var Color = require("color").Color;
 var CurrentPage;
-
-// Define GPS's Class
-var GPSData = new Observable({
-    PosX : "40.7696886",
-    PosY : "23.5531552"
-});
+var mapView;
+var justTrack = false;
 
 // Unit Functions
 function onNavigatingTo(args) {
     CurrentPage = args.object;
-    CurrentPage.bindingContext = GPSData;
     enableLocation();
 }
 exports.onNavigatingTo = onNavigatingTo;
@@ -23,66 +18,45 @@ function enableLocation(args) {
     if(!GeoLocation.isEnabled()) {
       GeoLocation.enableLocationRequest();
     }
-    trackLocation();
 }
 exports.enableLocation = enableLocation;
+
+function mapIsReady(args) {
+    mapView = args.object;
+    trackLocation();
+}
+exports.mapIsReady = mapIsReady;
 
 function trackLocation() {
   watchId = GeoLocation.watchLocation(
     function (loc) {
         if (loc) {
             console.log("Received location: " + loc.latitude + " " + loc.longitude);
-            GPSData.PosX = loc.latitude;
-            GPSData.PosY = loc.longitude;
+            mapView.latitude = loc.latitude;
+            mapView.longitude = loc.longitude;
+            if(justTrack) {
+              var marker = mapView.findMarker(function (marker) {
+                  return marker.userData.index === 1;
+              });
+              marker.position = mapsModule.Position.positionFromLatLng(mapView.latitude,mapView.longitude);
+            } else {
+              justTrack = true;
+              var marker = new mapsModule.Marker();
+              marker.position = mapsModule.Position.positionFromLatLng(mapView.latitude,mapView.longitude);
+              marker.title = "Me";
+              marker.userData = {index: 1};
+              mapView.addMarker(marker);
+            }
         }
     },
     function(e){
-        console.log("Error: " + e.message);
+        alert("Can't use GPS info.\nYou may see void map !!!");
     },
     {
       desiredAccuracy: 3,
       updateDistance: 10,
-      minimumUpdateTime : 1000 * 20
+      minimumUpdateTime : 1000 * 2
     }
   );
 }
 exports.trackLocation = trackLocation;
-
-function mapIsReady(args) {
-    showLocation(args);
-    showRadius(args);
-    generateQuest(args);
-}
-exports.mapIsReady = mapIsReady;
-
-function showLocation(args) {
-  var mapView = args.object;
-
-  var marker = new mapsModule.Marker();
-  marker.position = mapsModule.Position.positionFromLatLng(GPSData.PosX,GPSData.PosY);
-  marker.title = "Me";
-  marker.userData = {index: 1};
-  mapView.addMarker(marker);
-}
-
-function showRadius(args) {
-  var mapView = args.object;
-
-  var circle = new mapsModule.Circle();
-  circle.center = mapsModule.Position.positionFromLatLng(GPSData.PosX, GPSData.PosY);
-  circle.visible = true;
-  circle.radius = 100;
-  circle.fillColor = new Color('#99ff8800');
-  circle.strokeColor = new Color('#99ff0000');
-  circle.strokeWidth = 2;
-  mapView.addCircle(circle);
-}
-
-function generateQuest(args) {
-  var mapView = args.object;
-  var marker = new mapsModule.Marker();
-  marker.position = mapsModule.Position.positionFromLatLng(GPSData.PosX + 0.001,GPSData.PosY + 0.001);
-  marker.title = "Quest";
-  marker.userData = {index: 2};
-  mapView.addMarker(marker);
-}
