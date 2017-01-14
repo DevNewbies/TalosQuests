0d67907be5efff609cb789242ececf6264be6fb5
Danis Kostelidis <iordkost@teicm.gr>
2017-01-14 17:07:22 +0200

    Settings Working

--- Talos Quests/app/views/settings/settings.js
+++ Talos Quests/app/views/settings/settings.js
@@ -1,7 +1,13 @@
 var Observable = require("data/observable").Observable;
 var CurrentPage;
 
+var Settings = new Observable({
+  SoundEffectsEnabled : true,
+  MusicEnabled : true
+});
+
 function onNavigatingTo(args) {
     CurrentPage = args.object;
+    CurrentPage.bindingContext = Settings;
 }
 exports.onNavigatingTo = onNavigatingTo;
