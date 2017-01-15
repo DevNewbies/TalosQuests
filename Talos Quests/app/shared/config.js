996441d977381a42e286641921fbdde4eff5deb7
Danis Kostelidis <iordkost@teicm.gr>
2017-01-15 15:01:02 +0200

    Minor fixes

--- Talos Quests/app/shared/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	apiUrl: "http://service.talosquests.devian.gr/"
-};
--- Talos Quests/app/views/maps/maps.js
+++ Talos Quests/app/views/maps/maps.js
@@ -3,12 +3,14 @@ var Observable = require("data/observable").Observable;
 var GeoLocation = require("nativescript-geolocation");
 var mapsModule = require("nativescript-google-maps-sdk");
 var Color = require("color").Color;
+var disabledLocation = false;
 var CurrentPage;
 var mapView;
 
 var UserLocation = new Observable({
     userLatitude: 11.00000,
     userLongitude: 11.00000,
+    userZoom: 17,
     userLocated: false
 });
 
@@ -22,14 +24,17 @@ exports.onNavigatingTo = onNavigatingTo;
 
 function enableLocation() {
     if (!GeoLocation.isEnabled()) {
-        GeoLocation.enableLocationRequest();
+        alert("Can't use GPS info.\nYou may see void map !!!");
+        disabledLocation = true;
     }
 }
 exports.enableLocation = enableLocation;
 
 function mapIsReady(args) {
     mapView = args.object;
-    trackLocation();
+    if (!disabledLocation) {
+        trackLocation();
+    }
 }
 exports.mapIsReady = mapIsReady;
 
@@ -40,6 +45,7 @@ function trackLocation() {
                 console.log("Received location: " + loc.latitude + " " + loc.longitude);
                 CurrentPage.userLatitude = loc.latitude;
                 CurrentPage.userlongitude = loc.longitude;
+                CurrentPage.userZoom = 17;
                 CurrentPage.userLocated = true;
                 findMe();
             }
@@ -59,6 +65,7 @@ function findMe() {
     if (CurrentPage.userLocated == true) {
         mapView.latitude = CurrentPage.userLatitude;
         mapView.longitude = CurrentPage.userlongitude;
+        mapView.zoom = CurrentPage.userZoom;
 
 
         var UserMarker = new mapsModule.Marker();
@@ -74,6 +81,17 @@ function findMe() {
             index: 1
         };
         mapView.addMarker(UserMarker);
+
+        var UserCircle = new mapsModule.Circle();
+        mapView.removeAllCircles();
+
+        UserCircle.center = mapsModule.Position.positionFromLatLng(CurrentPage.userLatitude, CurrentPage.userlongitude);
+        UserCircle.visible = true;
+        UserCircle.radius = 50;
+        UserCircle.fillColor = new Color('#99ff8800');
+        UserCircle.strokeColor = new Color('#99ff0000');
+        UserCircle.strokeWidth = 2;
+        mapView.addCircle(UserCircle);
     }
 }
 exports.findMe = findMe;
