ad80d0a88e93f58bda71bc50ae2c7c3be8441851
Danis Kostelidis <iordkost@teicm.gr>
2017-01-15 12:25:55 +0200

    Update of Tabbed UI

--- Talos Quests/app/views/maps/maps.js
+++ Talos Quests/app/views/maps/maps.js
@@ -5,12 +5,11 @@ var mapsModule = require("nativescript-google-maps-sdk");
 var Color = require("color").Color;
 var CurrentPage;
 var mapView;
-var justTrack = false;
-var ChangedTab = false;
 
 var UserLocation = new Observable({
-  userLatitude : 11.00000,
-  userLongitude : 11.00000
+    userLatitude: 11.00000,
+    userLongitude: 11.00000,
+    userLocated: false
 });
 
 // Unit Functions
@@ -41,6 +40,7 @@ function trackLocation() {
                 console.log("Received location: " + loc.latitude + " " + loc.longitude);
                 CurrentPage.userLatitude = loc.latitude;
                 CurrentPage.userlongitude = loc.longitude;
+                CurrentPage.userLocated = true;
                 findMe();
             }
         },
@@ -56,37 +56,31 @@ function trackLocation() {
 exports.trackLocation = trackLocation;
 
 function findMe() {
-  mapView.latitude = CurrentPage.userLatitude;
-  mapView.longitude = CurrentPage.userlongitude;
+    if (CurrentPage.userLocated == true) {
+        mapView.latitude = CurrentPage.userLatitude;
+        mapView.longitude = CurrentPage.userlongitude;
 
 
-  var UserMarker = new mapsModule.Marker();
-  mapView.findMarker(function (marker) {
-            console.log(marker.userData.index);
-            if(marker.userData.index == 1) {
-              mapView.removeMarker(marker);
+        var UserMarker = new mapsModule.Marker();
+        mapView.findMarker(function(marker) {
+            if (marker.userData.index == 1) {
+                mapView.removeMarker(marker);
             }
         });
 
-  UserMarker.position = mapsModule.Position.positionFromLatLng(CurrentPage.userLatitude,CurrentPage.userlongitude);
-  UserMarker.title = "Me";
-  UserMarker.userData = {index: 1};
-  mapView.addMarker(UserMarker);
+        UserMarker.position = mapsModule.Position.positionFromLatLng(CurrentPage.userLatitude, CurrentPage.userlongitude);
+        UserMarker.title = "My Location";
+        UserMarker.userData = {
+            index: 1
+        };
+        mapView.addMarker(UserMarker);
+    }
 }
 exports.findMe = findMe;
 
-function onMarkerSelect(args) {
-  mapView.removeMarker(args.marker);
-}
-exports.onMarkerSelect = onMarkerSelect;
-
-
-function onSelectedIndexChanged(args) {
-  if(args.object.selectedIndex === 0 && ChangedTab) {
+function changeTab(args) {
+    if (CurrentPage.userLocated == true) {
         findMe();
-    ChangedTab = false;
-  } else {
-    ChangedTab = true;
-  }
+    }
 }
-exports.onSelectedIndexChanged = onSelectedIndexChanged;
+exports.changeTab = changeTab;
